20bf69ada0b6ab8c1c2c75e448d09dc3e33f5b77
ytyassen936-stack <ytyassen936@gmail.com>
2026-04-26 20:26:24 +0300

    congig. py

---
+++ config.py
@@ -0,0 +1,12 @@
+   BOT_TOKEN = "8678753090:AAEwqJz2YgEsdVYGFcjUC_NwdfAU1Y3nyfo"
+   ADMIN_ID = 7493679412
+   CHANNEL_USERNAME = "@w_3_vv"
+
+   WELCOME_MSG = """
+   اهلاً {name} 👋
+
+   انا بوت Dark AB
+   ضيفني بكروبك وخليني ادمن علمود اشتغل
+   """
+
+   DB_NAME = "bot_ab.db"
