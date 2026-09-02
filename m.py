c32d827eeddaa890e08a3e0f0d23fb37107137b6
ytyassen936-stack <ytyassen936@gmail.com>
2026-09-02 09:48:42 +0300

    Change BOT_TOKEN to a new value
    Updated BOT_TOKEN environment variable.

--- m.py
+++ m.py
@@ -31,7 +31,7 @@ except ImportError:
 
 API_ID = int(os.environ.get("API_ID", 34733680))
 API_HASH = os.environ.get("API_HASH", "dc47a14a8d693f8afbb73237d2ad7de8")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8989979653:AAG15pSehmpYOcO6vQcFZCMtNMzgQ3co4HQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8766360875:AAFUL_3pXZ8MdKoTeusTmKOJh6aKGte26vw")
 
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 7493679412))
 DEV_USERNAME = os.environ.get("DEV_USERNAME", "XX7X6")
